6bc474f6f5ebd2c8626190a70cbc9ead26e24bf5
cz-frontend <cz-frontend@163.com>
2023-03-07 16:40:56 +0800

    test: eventBus.on测试脚本

---
+++ test/event-bus.js
@@ -0,0 +1,9 @@
+const CZEventBus = require('../src/event-bus');
+
+const eventBus = new CZEventBus();
+
+// .on测试脚本
+const onThemeFn = (...payload) => {
+  console.log('--->on', payload);
+};
+console.log(eventBus.on('themeProps', onThemeFn));
