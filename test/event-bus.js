a4dc19ff14c64ed1efa289333a55bb5086e4904a
cz-frontend <cz-frontend@163.com>
2023-03-07 17:04:34 +0800

    test: .off测试脚本

--- test/event-bus.js
+++ test/event-bus.js
@@ -4,9 +4,16 @@ const eventBus = new CZEventBus();
 
 // .on测试脚本
 const onThemeFn = (...payload) => {
-  console.log('--->on', payload);
+  console.log('--->on', '执行了');
 };
 console.log(eventBus.on('themeProps', onThemeFn));
 
 // .emit测试脚本
 eventBus.emit('themeProps', 'dark');
+
+// .off测试脚本
+eventBus.on('off', onThemeFn);
+
+setTimeout(() => {
+  eventBus.off('off', onThemeFn);
+}, 1200);
